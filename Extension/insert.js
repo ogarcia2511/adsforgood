5c0b5f15a4bc878b60a55d06c67cc66ca92e46ad
Steven Bucher <stevenbucher8@gmail.com>
2020-02-22 15:23:04 -0800

    Delete insert.js

--- Extension/insert.js
+++
@@ -1,4 +0,0 @@
-var appbar = document.getElementById('appbar');
-var node = document.createElement("div");
-node.innerHTML = "<iframe src='https://adsforgood.dallen.io/ad?user=10000'></iframe>";
-appbar.appendChild(node);
