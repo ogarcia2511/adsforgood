a7259bee879da444cff1c64c452874cea98918d0
Donovan Allen <me@donovanallen.net>
2020-02-22 15:59:03 -0800

    Move ad

--- Extension/content.js
+++ Extension/content.js
@@ -1,9 +1,7 @@
-// var images = document.getElementsByTagName('img');
-
 var appbar = document.getElementById('appbar');
 
 var node = document.createElement("div");
 
-node.innerHTML = "<iframe src='https://adsforgood.dallen.io/ad?user=10000'></iframe>";
+node.innerHTML = "<iframe width=\"468\" height=\"60\" src=\"https://adsforgood.dallen.io/ad?user=10000\" style=\"border: none;overflow: hidden;padding-left: 12em;\"></iframe>";
 
 appbar.appendChild(node);
