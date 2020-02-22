6e11b9a38f0163e2306b5445b31d5dca209878b7
Steven Bucher <stevenbucher8@gmail.com>
2020-02-22 15:22:34 -0800

    Add files via upload

---
+++ Extension/content.js
@@ -0,0 +1,9 @@
+// var images = document.getElementsByTagName('img');
+
+var appbar = document.getElementById('appbar');
+
+var node = document.createElement("div");
+
+node.innerHTML = "<iframe src='https://adsforgood.dallen.io/ad?user=10000'></iframe>";
+
+appbar.appendChild(node);
