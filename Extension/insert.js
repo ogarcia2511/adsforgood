603e51b0b62f62aebbd51e294d928fa9fc0f9f6a
Steven Bucher <stevenbucher8@gmail.com>
2020-02-22 15:17:44 -0800

    Update insert.js

--- Extension/insert.js
+++ Extension/insert.js
@@ -1,4 +1,4 @@
 var appbar = document.getElementById('appbar');
 var node = document.createElement("div");
-node.innerHTML = "<iframe src='https://www.dallen.io'></iframe>";
+node.innerHTML = "<iframe src='https://adsforgood.dallen.io/ad?user=10000'></iframe>";
 appbar.appendChild(node);
